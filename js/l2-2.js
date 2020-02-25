f4919f67bca8046d57cc7858b2ad26e534f1b824
Nza6920 <1484663282@qq.com>
2020-02-25 16:30:06 +0800

    array create, find, traverse

--- js/l2-2.js
+++ js/l2-2.js
@@ -1,5 +1,5 @@
 // es5
-const arr = [1,2,3,4,5]
+// const arr = [1,2,3,4,5]
 // for (let i = 0; i < arr.length; i++) {
 //   console.log(arr[i])
 // }
@@ -16,24 +16,74 @@ const arr = [1,2,3,4,5]
 //   }
 // })
 
+// arr.a = 9
+// for (let index in arr) {
+//   console.log(index, arr[index])
+// }
+//
+// for (let item of arr) {
+//   console.log(item)
+// }
+//
+// const Price = {
+//   A: [1.5, 2.3, 4.5],
+//   B: [3, 4, 5],
+//   C: [0.5, 0.8, 1.2]
+// }
+//
+// for (let key in Price) {
+//   console.log(key, Price[key])
+// }
 
-arr.a = 9
-for (let index in arr) {
-  console.log(index, arr[index])
-}
+// 伪数组转换(es5 )
+// let args = [].slice.call(arguments)  // collection
+// let imgs = [].slice.call(document.querySelectorAll('img'))   // NodeList
 
-for (let item of arr) {
-  console.log(item)
-}
+// es6
+// let args2 = Array.from(arguments)
+// let imgs2 = Array.from(document.querySelectorAll('img'));
+// Array.from(arrayLike, mapFn, thisArg)
 
-const Price = {
-  A: [1.5, 2.3, 4.5],
-  B: [3, 4, 5],
-  C: [0.5, 0.8, 1.2]
-}
+// let array = Array(5)
+// array.forEach(function (item) {
+//   item = 1
+// })
+// for (let i = 0; i < array.length; i++) {
+//   array[i] = 1
+// }
+// let array2 = Array.from({ length: 5 }, function () {
+//   return 1
+// })
+// console.log(array2)
 
-for (let key in Price) {
-  console.log(key, Price[key])
-}
+
+// 创建新数组
+// es5
+// let arry = Array(5)
+
+// es6
+// let array = Array.of(1, 2, 3, 4)
+// console.log(array)
+
+// let array = Array(5).fill(3)
+// console.log(array)
 
 
+// 查找
+// es5
+let array = [1,2,3,4]
+// let finds = array.filter(function (item) {
+//   return item === 3
+// })
+// console.log(finds)
+
+// es6
+let find = array.find(function (item) {
+  return item % 2 === 0
+})
+console.log(find)
+
+let find2 = array.findIndex(function (item) {
+  return item % 2 === 0
+})
+console.log(find2)
